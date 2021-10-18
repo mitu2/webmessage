c404032fd9b6838d2225015797ffe2cbab87020c
mitu2 <chenmoand@outlook.com>
2021-10-18 20:46:52 +0800

    *

--- src/main/java/com/brageast/project/webmessage/config/websocket/WebSocketConfig.java
+++ src/main/java/com/brageast/project/webmessage/config/websocket/WebSocketConfig.java
@@ -32,6 +32,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
             webSocketHandlerRegistration.setAllowedOrigins("http://localhost:8090");
 
         }
+        webSocketHandlerRegistration.setAllowedOrigins("http://webmessage.static.run", "http://static.run");
         SockJsServiceRegistration sockJS = webSocketHandlerRegistration
                 //NOTE: 似乎不让用通配符 * 而新版Spring也没开放setAllowedOriginPatterns使用权限
                 .withSockJS();
